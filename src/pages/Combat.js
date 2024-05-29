e23a4454417331d94912fdb8031bc839b9cd86aa
KaisChell62 <148044856+KaisChell62@users.noreply.github.com>
2024-05-29 17:22:29 +0200

    test

--- src/pages/Combat.js
+++ src/pages/Combat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../css/Combat.css';
 
 function Combat() {
   const [pokemonList, setPokemonList] = useState([]);
